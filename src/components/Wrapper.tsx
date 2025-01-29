91e5423c1f2d9187b94e3d40ad471108e8acf0bf
Manfred Michaelis <mm@michm.de>
2025-01-29 01:14:27 +0100

    fix: default fallback causes no issues anymore

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -7,8 +7,10 @@ interface WrapperProps extends React.PropsWithChildren {
 }
 
 export const Wrapper: React.FC<WrapperProps> = ({ size, children, className }) => {
-    const classes = React.useMemo(() => className?.split(' ') ?? [], [className]);
-    const maxSize = React.useMemo(() => `max-w-${size ?? '7xl'}`, [size]);
+    // I think this is not necessary anymore in React v19. The compiler should be able to handle 
+    // this case pretty well. But I'm not sure. I have to test it.
+    const classes = React.useMemo(() => className?.split(' ') || [], [className]);
+    const maxSize = React.useMemo(() => `max-w-${size || '7xl'}`, [size]);
 
     return (
         <div className={clsx('py-4', 'xl:px-2', 'px-8', 'mx-auto', maxSize, ...classes)}>
