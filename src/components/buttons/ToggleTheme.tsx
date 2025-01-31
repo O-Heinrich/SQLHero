8760ed74982fbc85ff6532030df0fee34012cc71
Manfred Michaelis <mm@michm.de>
2025-01-31 21:04:42 +0100

    fix: Previous tsx resulted in encapsulated buttons

--- src/components/buttons/ToggleTheme.tsx
+++ src/components/buttons/ToggleTheme.tsx
@@ -1,16 +1,12 @@
 import { motion } from "motion/react"
 import { useTheme } from "@/hooks/useTheme";
 import { Sun, Moon } from "@/components/icons";
-import {Button} from "@headlessui/react";
 
 export const ToggleThemeButton: React.FC = () => {
     const { theme, toggleTheme } = useTheme();
     return (
-        
-            <Button onClick={toggleTheme} style={{backgroundColor: 'transparent'}}  >
-                <motion.button className="p-1 roubded-full" whileTap={{ scale: 0.15 }}>
-                {theme === 'dark' ? <Sun size={8} /> : <Moon size={8} />}
-                </motion.button>
-            </Button>
+        <motion.button onClick={toggleTheme} className="p-1 rounded-full bg-transparent" whileTap={{ scale: 0.75 }}>
+            {theme === 'dark' ? <Sun size={8} /> : <Moon size={8} />}
+        </motion.button>
     );
 }
