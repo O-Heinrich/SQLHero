c6c619f60c8ca022431543ac297f554f0578e76a
Manfred Michaelis <michaelis.m@it.bbwi>
2025-02-03 15:05:24 +0100

    style: Implemented footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { Wrapper } from "@/components/Wrapper";
+import { useTheme } from "@/hooks/useTheme";
+
+export const Footer: React.FC = () => {
+    const { theme } = useTheme();
+    const isDarkMode = React.useMemo(() => theme === 'dark', [theme]);
+
+    return (
+        <footer className="flex justify-center items-center min-h-16 bg-gray-200 dark:bg-[#44475a] dark:text-white">
+            <Wrapper className="text-center">
+                <p>loving</p>
+                <img src={isDarkMode ? '/drk-rki-bbw-3-dark-b.png' : '/drk-rki-bbw-3.png'} alt="Logo Berlin Beta Works Inklusiv" className="max-h-42 inline-block" />
+            </Wrapper>
+        </footer>
+    );
+};
