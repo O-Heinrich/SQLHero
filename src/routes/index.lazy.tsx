f93f283b5e37f63bbb4aaba0f4d186572275aa64
Manfred Michaelis <michaelis.m@it.bbwi>
2025-02-03 15:23:41 +0100

    feat: Changed content of index route

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,4 +1,5 @@
 import { Wrapper } from '@/components/Wrapper';
+import { Button } from '@headlessui/react';
 import { createLazyFileRoute } from '@tanstack/react-router';
 
 export const Route = createLazyFileRoute('/')({
@@ -9,7 +10,10 @@ function Index() {
     return (
         <Wrapper>
             <title>SQL Hero - Startseite</title>
-            <h3>Welcome Home!</h3>
+            <h2>Teste dein SQL Wissen!</h2>
+            <p>Teste dein SQL Wissen mit SQL Hero, dem SQL Tool mit interaktiven Übungen.</p>
+            <p>Auf jeder Seite findest du eine Aufgabe. Beantworte die Aufgabe direkt auf der Seite. Starte den Test und du siehst, ob deine Lösung richtig ist.</p>
+            <Button>Start</Button>
         </Wrapper>
     )
 }
