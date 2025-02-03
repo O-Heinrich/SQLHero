4ebef2ae6b351859dfa5df9fb7733d7c374a88d7
Manfred Michaelis <michaelis.m@it.bbwi>
2025-02-03 15:15:39 +0100

    style: Implemented dedicated css class

--- src/components/buttons/ToggleTheme.tsx
+++ src/components/buttons/ToggleTheme.tsx
@@ -5,7 +5,7 @@ import { Sun, Moon } from "@/components/icons";
 export const ToggleThemeButton: React.FC = () => {
     const { theme, toggleTheme } = useTheme();
     return (
-        <motion.button onClick={toggleTheme} className="p-1 rounded-full bg-transparent" whileTap={{ scale: 0.75 }}>
+        <motion.button onClick={toggleTheme} className="p-1 theme-button" whileTap={{ scale: 0.75 }}>
             {theme === 'dark' ? <Sun size={8} /> : <Moon size={8} />}
         </motion.button>
     );
