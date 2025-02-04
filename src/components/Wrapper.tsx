ea9cb10f0d0287ede7f06c6c83a6399b6a1641c0
Manfred Michaelis <michaelis.m@it.bbwi>
2025-02-04 16:42:37 +0100

    style: Removed default vertical padding

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -38,7 +38,7 @@ export const Wrapper: React.FC<WrapperProps> = ({ children, className }: Wrapper
     const classes: string[] = React.useMemo(() => className?.split(' ') || [], [className]);
 
     return (
-        <div className={clsx('max-w-7xl', 'py-4', 'xl:px-2', 'px-8', 'mx-auto', ...classes)}>
+        <div className={clsx('max-w-7xl', 'py-4', 'xl:px-2', 'mx-auto', ...classes)}>
             {children}
         </div>
     );
