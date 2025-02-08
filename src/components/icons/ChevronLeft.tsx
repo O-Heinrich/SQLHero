f8f8999962a45567d8693cc15621132552ffa001
Manfred Michaelis <mm@michm.de>
2025-02-08 02:53:25 +0100

    fix: Updated inverse icon

--- src/components/icons/ChevronLeft.tsx
+++ src/components/icons/ChevronLeft.tsx
@@ -4,7 +4,7 @@ export const ChevronLeft: React.FC<IconProps> = ({
     fill = ICON_DEFAULT_PROPS.fill,
     size = ICON_DEFAULT_PROPS.size,
 }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke={fill} className={`size-${size}`}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width={`${size}rem`} height={`${size}rem`} strokeWidth={1.5} stroke={fill}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
     </svg>
 );
