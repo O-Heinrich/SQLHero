0640d53beb1b3337e9095245cf579a5230db7c25
Manfred Michaelis <mm@michm.de>
2025-02-16 01:44:32 +0100

    style: Fixed, blured footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -29,7 +29,7 @@ export const Footer: React.FC = (): React.ReactElement => {
     const isDarkMode = React.useMemo(() => theme === 'dark', [theme]);
 
     return (
-        <footer className="flex justify-center items-center min-h-16 bg-slate-200 dark:bg-slate-900 dark:text-white border-t border-gray-300 dark:border-slate-700 py-2">
+        <footer className="flex justify-center items-center min-h-16 backdrop-blur-xl z-100 sticky bg-slate-200/20 dark:bg-slate-900/20 dark:text-white border-t border-gray-300 dark:border-slate-700 py-2 bottom-0">
             <Wrapper className="flex justify-between items-center max-w-8xl">
                 <span className="text-sm/4 self-end ml-2 text-gray-500 dark:text-gray-400">
                     &copy; {new Date().getFullYear()} Berlin Beta Works Inklusiv
