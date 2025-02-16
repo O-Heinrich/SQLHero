2d7398c8f22d379bea17e6b3c3ca8c43c84fe622
Manfred Michaelis <mm@michm.de>
2025-02-16 17:48:50 +0100

    style: Improved flashing effect

--- src/components/buttons/FlashButton.tsx
+++ src/components/buttons/FlashButton.tsx
@@ -107,69 +107,84 @@ interface FlashButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>
  * />
  * ```
  */
-export const FlashButton: React.FC<FlashButtonProps> = (props: FlashButtonProps): React.ReactElement => (
-    <button
-        {...props}
-        className={clsx(
-            'group/button',
-            'overflow-hidden',
-            'flex justify-center items-center',
-            'border border-transparent',
-            'shadow-sm',
-            'dark:text-white/85 text-black/85',
-            'font-light',
-            'transition-all',
-            'duration-250',
-            'ease-in-out',
-            'rounded-md',
-            'focus:outline-none',
-            'focus:ring-2',
-            'focus:ring-offset-2',
-            'focus:ring-offset-gray-100',
-            'focus:ring-primary-500',
-            'disabled:opacity-50',
-            'disabled:cursor-not-allowed',
-            'transition',
-            'duration-150',
-            'ease-in-out',
-            'hover:opacity-90',
-            'active:opacity-80',
-            'focus:opacity-80',
-            'focus:ring',
-            'focus:ring-opacity-50',
-            'focus:ring-primary-500',
-            'focus:ring-offset-gray-100',
-            'focus:ring-offset-2',
-            props.className,
-            props.size === 'sm' && 'mx-2 my-4 px-2 py-1 text-xl',
-            props.size === 'md' && 'mx-3 my-6 px-6 py-2 text-2xl',
-            props.size === 'lg' && 'mx-4 my-8 px-8 py-3 text-3xl',
-            props.variant === 'primary' && 'bg-primary-600 hover:bg-primary-700 active:bg-primary-800 focus:bg-primary-800',
-            props.variant === 'secondary' && 'bg-gray-600 hover:bg-gray-700 active:bg-gray-800 focus:bg-gray-800',
-            props.variant === 'danger' && 'bg-red-600 hover:bg-red-700 active:bg-red-800 focus:bg-red-800',
-            props.variant === 'warning' && 'bg-yellow-600 hover:bg-yellow-700 active:bg-yellow-800 focus:bg-yellow-800',
-            props.variant === 'success' && 'bg-green-600 hover:bg-green-700 active:bg-green-800 focus:bg-green-800',
-            props.variant === 'info' && 'bg-blue-600 hover:bg-blue-700 active:bg-blue-800 focus:bg-blue-800',
-            props.disabled && 'opacity-50 cursor-not-allowed',
-        )}
-        disabled={props.disabled || props.loading}
-        onClick={props.onClick}
-    >
-        {props.icon && (
-            <span className={clsx('mr-2', props.loading && 'animate-spin')}>
-                {props.icon}
-            </span>
-        )}
-        
-        <span className={clsx(
-            props.size === 'sm' && 'text-xl',
-            props.size === 'md' && 'text-2xl',
-            props.size === 'lg' && 'text-3xl',
-        )}>{props.label}</span>
-        <div
-            className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-13deg)_translateX(-100%)] group-hover/button:duration-1000 group-hover/button:[transform:skew(-13deg)_translateX(100%)]"
+export const FlashButton: React.FC<FlashButtonProps> = (props: FlashButtonProps): React.ReactElement => {
+    const flashClasses = `
+    relative h-full w-10 
+    bg-white/60 dark:bg-white/10 
+    from-white/50 from-25%
+    via-white/60 via-30%  
+    to-white/30 
+    dark:from-white/8 dark:from-25%
+    dark:via-white/6 dark:via-30% 
+    dark:to-white/3
+    bg-gradient-to-l
+    opacity-80 dark:opacity-40
+    `;
+
+    return (
+        <button
+            {...props}
+            className={clsx(
+                'group/button',
+                'overflow-hidden',
+                'flex justify-center items-center',
+                'border border-transparent',
+                'shadow-sm',
+                'dark:text-white/85 text-black/85',
+                'font-light',
+                'transition-all',
+                'duration-250',
+                'ease-in-out',
+                'rounded-md',
+                'focus:outline-none',
+                'focus:ring-2',
+                'focus:ring-offset-2',
+                'focus:ring-offset-gray-100',
+                'focus:ring-primary-500',
+                'disabled:opacity-50',
+                'disabled:cursor-not-allowed',
+                'transition',
+                'duration-150',
+                'ease-in-out',
+                'hover:opacity-90',
+                'active:opacity-80',
+                'focus:opacity-80',
+                'focus:ring',
+                'focus:ring-opacity-50',
+                'focus:ring-primary-500',
+                'focus:ring-offset-gray-100',
+                'focus:ring-offset-2',
+                props.className,
+                props.size === 'sm' && 'mx-2 my-4 px-2 py-1 text-xl',
+                props.size === 'md' && 'mx-3 my-6 px-6 py-2 text-2xl',
+                props.size === 'lg' && 'mx-4 my-8 px-8 py-3 text-3xl',
+                props.variant === 'primary' && 'bg-primary-600 hover:bg-primary-700 active:bg-primary-800 focus:bg-primary-800',
+                props.variant === 'secondary' && 'bg-gray-600 hover:bg-gray-700 active:bg-gray-800 focus:bg-gray-800',
+                props.variant === 'danger' && 'bg-red-600 hover:bg-red-700 active:bg-red-800 focus:bg-red-800',
+                props.variant === 'warning' && 'bg-yellow-600 hover:bg-yellow-700 active:bg-yellow-800 focus:bg-yellow-800',
+                props.variant === 'success' && 'bg-green-600 hover:bg-green-700 active:bg-green-800 focus:bg-green-800',
+                props.variant === 'info' && 'bg-blue-600 hover:bg-blue-700 active:bg-blue-800 focus:bg-blue-800',
+                props.disabled && 'opacity-50 cursor-not-allowed',
+            )}
+            disabled={props.disabled || props.loading}
+            onClick={props.onClick}
         >
-            <div className="relative h-full w-10 bg-white/60"></div>
-        </div>
-    </button>
-)
+            {props.icon && (
+                <span className={clsx('mr-2', props.loading && 'animate-spin')}>
+                    {props.icon}
+                </span>
+            )}
+            
+            <span className={clsx(
+                props.size === 'sm' && 'text-xl',
+                props.size === 'md' && 'text-2xl',
+                props.size === 'lg' && 'text-3xl',
+            )}>{props.label}</span>
+            <div
+                className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-13deg)_translateX(-100%)] group-hover/button:duration-1000 group-hover/button:[transform:skew(-13deg)_translateX(100%)]"
+            >
+                <div className={flashClasses}></div>
+            </div>
+        </button>
+    )
+}
