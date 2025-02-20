5b642243c2754d09abb9f0929eda2d9ec9819c5b
Manfred Michaelis <mm@michm.de>
2025-02-20 01:10:59 +0100

    refactored: Updated design
    - Added Logos for Firefox, Chrome and Edge
    - Added Badge and Circle checkmark icon
    - Removed highlight.js

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -3,3 +3,5 @@ export { Moon } from "@/components/icons/Moon";
 export { ChevronRight } from "@/components/icons/ChevronRight";
 export { ChevronLeft } from "@/components/icons/ChevronLeft";
 export { Exclamation } from "@/components/icons/Exclamation";
+export { CheckBadge } from "@/components/icons/CheckBadge";
+export { CheckCircle } from "@/components/icons/CheckCircle";
