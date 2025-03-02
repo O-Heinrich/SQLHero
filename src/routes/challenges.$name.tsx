e1f8e8ebe7aaae8e2d41d2fca4defe7f188d8991
Manfred Michaelis <mm@michm.de>
2025-03-02 04:23:58 +0100

    fix: editor init

--- src/routes/challenges.$name.tsx
+++ src/routes/challenges.$name.tsx
@@ -349,8 +349,8 @@ function Challenge() {
     useEffect(() => {
         // When fast debuging, the editorState is set 
         // to the challenge query: setEditorState(() => challenge.query);
-        const index = state.challenges[challengeNo - 1].attempts.length - 1;
-        setEditorState(() => state.challenges[challengeNo - 1].attempts[index].query ?? '');
+        const index = state.challenges[challengeNo - 1]?.attempts?.length - 1;
+        setEditorState(() => state.challenges[challengeNo - 1]?.attempts[index]?.query ?? '');
         if (db !== challenge.schema) {
             fetch(challenge.schema).then(async (response) => {
                 try {
