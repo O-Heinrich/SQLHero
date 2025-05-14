f3b7e5e2db02e239d5cd7af47ab1c0495b5747b9
Manfred Michaelis <mm@michm.de>
2025-05-14 21:04:38 +0200

    fix(workaround): Strange error 'strict mode not allowed here' by adding the module javascript-engine.ts by Vite in dev mode is avoided

--- src/components/QueryResultTable.tsx
+++ src/components/QueryResultTable.tsx
@@ -18,6 +18,9 @@ import { useMemo } from "react";
 import { Table } from '@/components/table';
 import { QueryResult } from '@/lib/exec-engine/postgres-engine';
 import { queryResultToStringArray } from '@/lib/utils';
+// FIXME: This import is intentionally used as workaround for weird issue in dev mode
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { JavaScriptExecutionResult } from "@/lib/exec-engine/javascript-engine.ts";
 
 /**
  * Props interface for QueryResultTable component
