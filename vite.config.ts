a8101343327b8b2137fa0db2624a44ab3935d6a4
Manfred Michaelis <mm@michm.de>
2026-01-07 21:32:19 +0100

    chore: migrated from deprecated TanstackRouterVite to tanstackRouter

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,17 @@ import path from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import createChallenges from './tools/vite-create-challenges-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
-        react(),
         tailwindcss(),
-        TanStackRouterVite(),
+        tanstackRouter({
+            target: 'react',
+        }),
+        react(),
         createChallenges({
             path: 'challenges',
             output: 'public/api/challenges',
